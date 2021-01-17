cfd5a9d592061bb879953b993667c14430004f3a
donuts <donuts@42network.org>
2021-01-17 23:17:10 +0100

    Home page layout

---
+++ app/components/HomeReview.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-card style="position: relative; height: 100%">
+		<v-card-title style="font-size: 0.8em">
+			<h4 style="z-index: 1">{{ title }}</h4>
+			<v-icon x-large color="grey lighten-1" style="position: absolute; right: 0.1em">
+				mdi-format-quote-close
+			</v-icon>
+		</v-card-title>
+		<v-card-text>{{ content }}</v-card-text>
+	</v-card>
+</template>
+
+<script>
+	export default {
+		props: ['title', 'content'],
+	};
+</script>
+
+<style scoped></style>
---
+++ app/components/HomeReviews.vue
@@ -0,0 +1,35 @@
+<template>
+	<v-container>
+		<v-row>
+			<v-col cols="4" lg>
+				<HomeReview
+					title="Rebecca Stephenson"
+					content="THANK YOU for making it possible for me to meet my soulmate. Five minutes into our first
+						conversation, my now-wife mentioned how we would have an amazing wedding."
+				/>
+			</v-col>
+			<v-col cols="4" lg>
+				<HomeReview
+					title="Gabriel & Fiance"
+					content="I met my fiancé on tinder during the quarantine for COVID. She is from Lafayette, Indiana and I
+						am from Cleveland, Ohio. We are both Christians who fell madly in love."
+				/>
+			</v-col>
+			<v-col cols="4" lg>
+				<HomeReview
+					title="Shannon & Julian"
+					content="I was feeling lonely back in my hometown because most of my friends had started romantic
+						relationships while I was abroad. We both decided to download Tinder and see what happened.
+						Without the app we may have never met and embarked on this wild, wonderful journey. Thank you
+						for bringing us and so many other couples together around the world. I will forever be grateful."
+				/>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style scoped></style>
