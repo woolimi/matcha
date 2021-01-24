bdfdb8466b368c797501f7861a470dbcabf4d40d
donuts <donuts@42network.org>
2021-01-24 21:11:32 +0100

    fix charset

--- api/models/User.ts
+++ api/models/User.ts
@@ -16,16 +16,16 @@ class User extends Model {
 	static table =
 		"CREATE TABLE `users` ( \
 			`id` int NOT NULL AUTO_INCREMENT, \
-			`email` varchar(60) COLLATE utf8_bin NOT NULL, \
-			`username` varchar(20) COLLATE utf8_bin NOT NULL, \
-			`password` varchar(100) COLLATE utf8_bin NOT NULL, \
-			`lastName` varchar(45) COLLATE utf8_bin NOT NULL, \
-			`firstName` varchar(45) COLLATE utf8_bin NOT NULL, \
+			`email` varchar(60) NOT NULL, \
+			`username` varchar(20) NOT NULL, \
+			`password` varchar(100) NOT NULL, \
+			`lastName` varchar(45) NOT NULL, \
+			`firstName` varchar(45) NOT NULL, \
 			`verified` tinyint DEFAULT '0', \
 			PRIMARY KEY (`id`), \
 			UNIQUE KEY `email_UNIQUE` (`email`), \
 			UNIQUE KEY `username_UNIQUE` (`username`) \
-		) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin";
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
 	static init(): Promise<any> {
 		return Model.init('users', User);
 	}
