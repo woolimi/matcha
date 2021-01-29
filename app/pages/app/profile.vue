3af8e2328f291c20717961aeaf3a977bf61481b4
donuts <donuts@42network.org>
2021-01-29 23:29:44 +0100

    profile view prototype

--- app/pages/app/profile.vue
+++ app/pages/app/profile.vue
@@ -1,56 +1,20 @@
 <template>
 	<v-container>
-		<!-- Profile image -->
-		<v-form>
-			<v-row>
-				<v-col col="12" class="text-center"> image </v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center"> 1 / 2 / 3 / 4 / 5 </v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center">fame raiting</v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center"> location </v-col>
-			</v-row>
-		</v-form>
-
-		<!-- Private Info Form -->
-		<v-form>
-			<v-row>
-				<v-col col="12" class="text-center"> first name </v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center"> last name </v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center"> email </v-col>
-			</v-row>
-		</v-form>
-
-		<!-- Public Info Form -->
-		<v-form>
-			<v-row>
-				<v-col col="12" class="text-center"> gender </v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center"> Sexual preferences </v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center"> A biography. </v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center"> A list of interests with tags </v-col>
-			</v-row>
-			<v-row>
-				<v-col col="12" class="text-center"> A biography. </v-col>
-			</v-row>
-		</v-form>
+		<!-- Profile card -->
+		<v-row>
+			<v-col cols="12">
+				<ProfileCard />
+			</v-col>
+			<v-col cols="12">
+				<PublicInfo />
+			</v-col>
+		</v-row>
 	</v-container>
 </template>
 
 <script>
+	import { mapState } from 'vuex';
+
 	export default {
 		auth: true,
 	};
