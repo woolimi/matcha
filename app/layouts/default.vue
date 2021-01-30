dea9b8c6fd156e288c1de8901bf778784b12605b
Glagan <Glagan@users.noreply.github.com>
2021-01-30 14:01:48 +0100

    Fix scroll in chat

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,18 +1,9 @@
 <template>
-	<v-app>
+	<v-app :style="!isLandingPage ? { height: '100vh' } : ''">
 		<template v-if="this.$route.path === '/'">
 			<HomeNav app />
-			<div
-				class="align-center justify-center d-flex flex-column"
-				style="
-					background-image: url(/img/home-bg.jpg);
-					background-size: cover;
-					background-position: center center;
-					height: 100vh;
-					background-attachment: fixed;
-				"
-			>
-				<h1 class="white--text" style="font-size: 4em">Find your Love</h1>
+			<div class="align-center justify-center d-flex flex-column landing">
+				<h1 class="white--text">Find your Love</h1>
 				<v-dialog v-model="show" max-width="400px">
 					<template v-slot:activator="{ on, attrs }">
 						<v-btn v-bind="attrs" v-on="on" color="primary">Create an Account</v-btn>
@@ -24,7 +15,7 @@
 		<template v-else>
 			<AppNav app />
 		</template>
-		<v-main>
+		<v-main :style="!isLandingPage ? { overflow: 'auto', 'flex-shrink': '1' } : { padding: '64px 0' }">
 			<nuxt />
 		</v-main>
 		<Footer />
@@ -34,11 +25,22 @@
 
 <script>
 	export default {
+		data() {
+			return {
+				isLandingPage: true,
+			};
+		},
 		mounted() {
 			if (this.$auth.loggedIn) {
 				if (this.$store.state.refreshId) clearInterval(this.$store.state.refreshId);
 				this.$tokenManager.silentRefresh(this);
 			}
+			this.isLandingPage = this.$route.path == '/';
+		},
+		watch: {
+			$route(to, _from) {
+				this.isLandingPage = to.path == '/';
+			},
 		},
 		computed: {
 			show: {
@@ -52,3 +54,16 @@
 		},
 	};
 </script>
+
+<style scoped>
+	.landing {
+		background-image: url(/img/home-bg.jpg);
+		background-size: cover;
+		background-position: center center;
+		height: 100vh;
+		background-attachment: fixed;
+	}
+	.landing h1 {
+		font-size: 4em;
+	}
+</style>
