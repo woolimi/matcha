6ec9e40e2113c6f28956438595c56ccd45db1f62
Glagan <Glagan@users.noreply.github.com>
2021-02-06 12:37:22 +0100

    Move Chat to their own components

---
+++ app/components/App/chat/ChatList.vue
@@ -0,0 +1,76 @@
+<template>
+	<v-col cols="12" md="3" :class="classes">
+		<template v-if="chatList.length > 0">
+			<v-list width="100%">
+				<v-list-item-group>
+					<v-list-item v-for="chat in chatList" :key="chat.id" @click="selectChat">
+						<v-list-item-avatar>
+							<v-badge
+								:color="chat.user.online ? 'green' : 'pink'"
+								bordered
+								avatar
+								dot
+								bottom
+								offset-x="10"
+								offset-y="10"
+							>
+								<v-avatar size="40">
+									<v-img :src="chat.user.picture ? chat.user.picture : ''"></v-img>
+								</v-avatar>
+							</v-badge>
+						</v-list-item-avatar>
+						<v-list-item-content>
+							<v-list-item-title>
+								{{ chat.user.username }}
+							</v-list-item-title>
+							<v-list-item-subtitle
+								>{{ chat.last ? chat.last : 'No messages' }}
+								<!-- &mdash; {{ user.date.toLocaleTimeString() }} --></v-list-item-subtitle
+							>
+						</v-list-item-content>
+					</v-list-item>
+				</v-list-item-group>
+			</v-list>
+		</template>
+		<template v-else>
+			<div class="pa-2">
+				<v-alert border="left" elevation="2" outlined text type="info">
+					Start matching to chat with other peoples !
+				</v-alert>
+			</div>
+		</template>
+	</v-col>
+</template>
+
+<script>
+	export default {
+		computed: {
+			chatList() {
+				return this.$store.getters['chat/list'];
+			},
+			classes() {
+				return `${this.$store.getters['chat/chat'] === undefined ? 'd-flex' : 'hidden-sm-and-down'} chat-list`;
+			},
+		},
+		methods: {
+			selectChat(chat) {
+				//
+			},
+		},
+		mounted() {
+			this.$store.dispatch('chat/load');
+		},
+	};
+</script>
+
+<style scoped>
+	.chat-list {
+		height: 100%;
+		overflow: auto;
+		border-right: 1px solid rgba(0, 0, 0, 0.12);
+	}
+
+	.v-list-item .v-avatar {
+		overflow: visible;
+	}
+</style>
