d4f5b871b16698ec53cbdbf500bec661e86d6434
donuts <donuts@42network.org>
2021-02-10 22:55:23 +0100

    search filter

--- app/pages/app/search.vue
+++ app/pages/app/search.vue
@@ -1,13 +1,29 @@
 <template>
 	<div>
-		<h1>search</h1>
+		<v-expansion-panels accordion class="d-md-none">
+			<SmallSearchFilter />
+			<SmallSearchTag />
+		</v-expansion-panels>
 	</div>
 </template>
 
 <script>
 	export default {
 		auth: true,
+		data: () => ({
+			tags: {
+				selected: [],
+				items: ['a', 'b'],
+				search: null,
+			},
+			mode: 1,
+			open: true,
+		}),
 	};
 </script>
 
-<style scoped></style>
+<style scoped>
+	.h-init {
+		height: initial;
+	}
+</style>
