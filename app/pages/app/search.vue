c276244e3accd838cff37869caa23477cb338648
donuts <donuts@42network.org>
2021-02-13 00:02:13 +0100

    image map search mode

--- app/pages/app/search.vue
+++ app/pages/app/search.vue
@@ -5,6 +5,17 @@
 			<SmallSearchTag />
 		</v-expansion-panels>
 		<div style="height: 6.5em" class="d-md-none"></div>
+		<v-container class="d-flex justify-space-between align-center">
+			<span class="grey--text text--darken-2">Total 7 results</span>
+			<v-btn-toggle mandatory v-model="mode">
+				<v-btn value="image">
+					<v-icon> mdi-image-multiple-outline </v-icon>
+				</v-btn>
+				<v-btn value="map">
+					<v-icon> mdi-map-search-outline </v-icon>
+				</v-btn>
+			</v-btn-toggle>
+		</v-container>
 		<Results />
 	</div>
 </template>
@@ -13,7 +24,7 @@
 	export default {
 		auth: true,
 		data: () => ({
-			mode: 1,
+			mode: 'image',
 			open: true,
 		}),
 	};
