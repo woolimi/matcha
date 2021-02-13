be7bedfa7b25b9250160dbc0abd4a8413570e994
donuts <donuts@42network.org>
2021-02-14 00:23:58 +0100

    finish UI

---
+++ app/components/App/search/SearchMode.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-container class="d-flex justify-space-between align-center">
+		<span class="grey--text text--darken-2">Total 7 results</span>
+		<v-btn-toggle mandatory v-model="mode">
+			<v-btn value="image">
+				<v-icon> mdi-image-multiple-outline </v-icon>
+			</v-btn>
+			<v-btn value="map">
+				<v-icon> mdi-map-search-outline </v-icon>
+			</v-btn>
+		</v-btn-toggle>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		data: () => ({
+			mode: 'image',
+		}),
+	};
+</script>
+
+<style scoped></style>
---
+++ app/components/App/search/SearchTag.vue
@@ -0,0 +1,36 @@
+<template>
+	<v-container>
+		<v-card elevation="4">
+			<v-card-text>
+				<v-subheader class="pa-0 h-init font-weight-bold">Tags</v-subheader>
+				<v-autocomplete
+					v-model="tags.selected"
+					chips
+					multiple
+					hide-selected
+					auto-select-first
+					@change="tags.search = null"
+					:items="tags.items"
+					:search-input.sync="tags.search"
+				>
+				</v-autocomplete>
+			</v-card-text>
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tags: {
+					selected: [],
+					items: ['a', 'b'],
+					search: null,
+				},
+			};
+		},
+	};
+</script>
+
+<style scoped></style>
