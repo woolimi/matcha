bb10d0064de67d3ce1e48fcc5db328995526eb2f
donuts <donuts@42network.org>
2021-02-14 00:23:58 +0100

    finish UI

---
+++ app/components/App/search/SearchFilter.vue
@@ -0,0 +1,46 @@
+<template>
+	<v-container>
+		<v-card elevation="4">
+			<v-card-text>
+				<v-subheader class="pa-0 h-init font-weight-bold">
+					<span>Age</span>
+					<v-spacer></v-spacer>
+					<span>{{ age[0] }} - {{ age[1] }}</span>
+				</v-subheader>
+				<v-range-slider dense v-model="age" min="18" max="50"> </v-range-slider>
+				<v-subheader class="pa-0 h-init font-weight-bold">
+					<span>Distance</span>
+					<v-spacer></v-spacer>
+					<span>{{ distance[0] }} - {{ distance[1] }} km</span>
+				</v-subheader>
+				<v-range-slider dense v-model="distance" min="0" max="100"></v-range-slider>
+				<v-subheader class="pa-0 h-init font-weight-bold">
+					<span> Likes </span>
+					<v-spacer></v-spacer>
+					<span>{{ likes[0] }} - {{ likes[1] }}</span>
+				</v-subheader>
+				<v-range-slider dense v-model="likes" min="0" max="100"></v-range-slider>
+			</v-card-text>
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				age: [18, 30],
+				distance: [0, 30],
+				likes: [0, 30],
+				tags: {
+					selected: [],
+					items: ['a', 'b'],
+					search: null,
+				},
+				mode: 1,
+			};
+		},
+	};
+</script>
+
+<style scoped></style>
--- app/pages/app/search.vue
+++ app/pages/app/search.vue
@@ -1,23 +1,23 @@
 <template>
-	<div>
+	<v-container class="pa-0">
 		<v-expansion-panels accordion class="d-md-none" style="position: fixed">
 			<SmallSearchFilter />
 			<SmallSearchTag />
 		</v-expansion-panels>
 		<div style="height: 6.5em" class="d-md-none"></div>
-		<v-container class="d-flex justify-space-between align-center">
-			<span class="grey--text text--darken-2">Total 7 results</span>
-			<v-btn-toggle mandatory v-model="mode">
-				<v-btn value="image">
-					<v-icon> mdi-image-multiple-outline </v-icon>
-				</v-btn>
-				<v-btn value="map">
-					<v-icon> mdi-map-search-outline </v-icon>
-				</v-btn>
-			</v-btn-toggle>
+		<v-container>
+			<v-row>
+				<v-col class="d-none d-md-block" cols="4">
+					<SearchFilter />
+					<SearchTag />
+				</v-col>
+				<v-col cols="12" md="8">
+					<SearchMode />
+					<Results />
+				</v-col>
+			</v-row>
 		</v-container>
-		<Results />
-	</div>
+	</v-container>
 </template>
 
 <script>
