0281a3b06f8c0c96cf8b8cb4732c1dbe68d05e19
Glagan <Glagan@users.noreply.github.com>
2021-02-16 19:03:08 +0100

    Public profile mockup

--- app/components/App/notifications/LikeReceived.vue
+++ app/components/App/notifications/LikeReceived.vue
@@ -9,7 +9,7 @@
 				{{ row.user.username }} liked your profile.
 				<v-tooltip bottom>
 					<template v-slot:activator="{ on, attrs }">
-						<v-btn :to="`/app/user/${row.user.id}`" color="action" small icon v-bind="attrs" v-on="on">
+						<v-btn :to="`/app/users/${row.user.id}`" color="action" small icon v-bind="attrs" v-on="on">
 							<v-icon>mdi-arrow-right</v-icon>
 						</v-btn>
 					</template>
@@ -19,12 +19,15 @@
 			<v-list-item-subtitle> {{ new Date(row.at).toLocaleString() }} </v-list-item-subtitle>
 		</v-list-item-content>
 
-		<MarkAsRead v-if="!row.status" :id="row.id" />
+		<MarkAsRead v-if="manageable && !row.status" :id="row.id" />
 	</v-list-item>
 </template>
 
 <script>
 	export default {
-		props: { row: { type: Object, required: true } },
+		props: {
+			row: { type: Object, required: true },
+			manageable: { type: Boolean, required: true },
+		},
 	};
 </script>
