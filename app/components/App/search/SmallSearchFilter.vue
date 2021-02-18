9e09d5dd73e53d6b213fc1f78258b9a0c959623a
donuts <donuts@42network.org>
2021-02-18 02:06:28 +0100

    watch filter fin

--- app/components/App/search/SmallSearchFilter.vue
+++ app/components/App/search/SmallSearchFilter.vue
@@ -49,33 +49,9 @@
 </template>
 
 <script>
+	import SearchFilterMixin from '~/mixins/SearchFilterMixin';
 	export default {
-		computed: {
-			age: {
-				get() {
-					return this.$store.state.search.age;
-				},
-				set(val) {
-					this.$store.commit('search/SET_AGE', val);
-				},
-			},
-			distance: {
-				get() {
-					return this.$store.state.search.distance;
-				},
-				set(val) {
-					this.$store.commit('search/SET_DISTANCE', val);
-				},
-			},
-			likes: {
-				get() {
-					return this.$store.state.search.likes;
-				},
-				set(val) {
-					this.$store.commit('search/SET_LIKES', val);
-				},
-			},
-		},
+		mixins: [SearchFilterMixin],
 	};
 </script>
 
