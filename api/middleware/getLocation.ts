17215774f36af03c3bd682cdc9412b6a97cb9c4f
donuts <donuts@42network.org>
2021-02-24 00:35:25 +0100

    add getLocation middleware

---
+++ api/middleware/getLocation.ts
@@ -0,0 +1,19 @@
+import { NextFunction } from 'express';
+import _ from 'lodash';
+import geoip from 'geoip-lite';
+
+const getLocation = (req: any, res: any, next: NextFunction): any => {
+	const uinfo = req.body;
+	const clientIp = req.clientIp;
+	const default_location = { lat: 48.8566, lng: 2.3522 };
+	// Find location by IP if browser gps diabled
+	if (!_.isEmpty(uinfo.location)) return next();
+	if (!clientIp) return default_location;
+
+	const found_ll = geoip.lookup(clientIp)?.ll;
+	uinfo.location = found_ll ? { lat: found_ll[0], lng: found_ll[1] } : default_location;
+
+	return next();
+};
+
+export default getLocation;
