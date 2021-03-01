dd7db8efef184b758a4608e4d7e2911a49209d85
donuts <donuts@42network.org>
2021-03-01 23:27:16 +0100

    app prod set

--- app/plugins/client-oauth2.js
+++ app/plugins/client-oauth2.js
@@ -3,7 +3,7 @@ import ClientOAuth2 from 'client-oauth2';
 const googleAuth = new ClientOAuth2({
 	clientId: '342917151081-omsbull5ubgg8sgtgj3i90v263irib1j.apps.googleusercontent.com',
 	authorizationUri: 'https://accounts.google.com/o/oauth2/v2/auth',
-	redirectUri: 'http://localhost:3000/callback',
+	redirectUri: `${process.env.APP}/callback`,
 	scopes: ['email', 'profile'],
 });
 
--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-	baseURL: `http://localhost:8080`,
+	baseURL: `${process.env.API}`,
 	withCredentials: false,
 	headers: {
 		Accept: 'application/json',
